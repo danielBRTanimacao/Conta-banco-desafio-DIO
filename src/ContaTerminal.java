737e3b791489d0ece83b63b7e86154aefde8d15c
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-04-02 10:23:07 -0300

    feat: created logical

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        int number = scan.nextInt();
+        System.out.println("Agora a sua Agência!");
+        String agent = scan.next();
+        System.out.println("Seu nome");
+        String clientName = scan.next();
+        System.out.println("Digite seu saldo");
+        double balance = scan.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f%n já está disponível para saque.", clientName, agent, number, balance);
+        scan.close();
     }
 }
